446d6d7917a7b24af57ea3eb5cddc7475b8532d2
Dave Redmin <david.redmin@beta.dhs.gov>
2019-10-18 13:31:10 -0400

    Fix test to handle Fedora, which shows the installed package name as "python2-libselinux" rather than "libselinux-python"

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["python2-libselinux"])
-def test_packages(host, pkg):
+@pytest.mark.parametrize(
+    "pkg,fedora_pkg", [("libselinux-python", "python2-libselinux")]
+)
+def test_packages(host, pkg, fedora_pkg):
     """Test that the appropriate packages were installed."""
-    assert host.package(pkg).is_installed
+    if host.system_info.distribution == "fedora":
+        assert host.package(fedora_pkg).is_installed
+    else:
+        assert host.package(pkg).is_installed

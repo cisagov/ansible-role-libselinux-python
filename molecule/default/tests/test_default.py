d570265eb0205497c84510fc7f7ffe06872dee77
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-12-17 18:31:12 -0500

    Update role and testing
    Update the role to use var files for each OS distribution. Update tests to
    reflect these changes.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,7 +4,6 @@
 import os
 
 # Third-Party Libraries
-import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -12,12 +11,28 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize(
-    "pkg,fedora_pkg", [("libselinux-python", "python2-libselinux")]
-)
-def test_packages(host, pkg, fedora_pkg):
+def test_packages(host):
     """Test that the appropriate packages were installed."""
-    if host.system_info.distribution == "fedora":
-        assert host.package(fedora_pkg).is_installed
+    pkgs = None
+    print()
+    if (
+        host.system_info.distribution == "debian"
+        and host.system_info.codename == "stretch"
+    ):
+        pkgs = ["python-selinux", "python3-selinux"]
+    elif (
+        host.system_info.distribution == "debian"
+        or host.system_info.distribution == "ubuntu"
+        or host.system_info.distribution == "kali"
+    ):
+        pkgs = ["python3-selinux"]
+    elif host.system_info.distribution == "fedora":
+        pkgs = ["python3-libselinux"]
+    elif host.system_info.distribution == "amzn":
+        pkgs = ["libselinux-python"]
     else:
+        # This is an unknown OS, so force the test to fail
+        assert False
+
+    for pkg in pkgs:
         assert host.package(pkg).is_installed
